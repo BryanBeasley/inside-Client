a9313c65db90a86dae78c8b1c4ba24ac959bf835
BryanBeasley <78266471+BryanBeasley@users.noreply.github.com>
2021-07-31 05:50:51 -0400

    launching client side

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -1,10 +1,12 @@
 let APIURL = "";
 
 switch (window.location.hostname) {
-    case "localhost":
-    case "127.0.0.1":
-        APIURL= "http://localhost:3030";
-
+  case "localhost":
+  case "127.0.0.1":
+    APIURL = "http://localhost:3030";
+    break;
+  case "rbclientinside.herokuapp.com":
+    APIURL = "https://rbservertwo.herokuapp.com";
 }
 
 export default APIURL;
